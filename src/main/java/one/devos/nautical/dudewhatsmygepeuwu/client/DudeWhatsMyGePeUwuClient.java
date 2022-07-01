8c8e3d292100f7ee24e51a7c73da774c4c300559
Stereo528 <52116771+Stereo528@users.noreply.github.com>
2022-07-01 12:13:01 -0600

    Code Cleanup, Says GPU on Window Title
    Window Title only on main menu, ran out of time to figure out how to make it game wide

--- src/main/java/one/devos/nautical/dudewhatsmygepeuwu/client/DudeWhatsMyGePeUwuClient.java
+++ src/main/java/one/devos/nautical/dudewhatsmygepeuwu/client/DudeWhatsMyGePeUwuClient.java
@@ -7,15 +7,11 @@ import org.slf4j.LoggerFactory;
 
 public class DudeWhatsMyGePeUwuClient implements ClientModInitializer {
     public static final String MOD_ID = "dudewhatsmygepeuwu";
-    public static final String VERSION = FabricLoader.getInstance().getModContainer(MOD_ID).get().getMetadata().getVersion().getFriendlyString();
-    public static final String MOD_NAME = FabricLoader.getInstance().getModContainer(MOD_ID).get().getMetadata().getName();
-
-    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_NAME);
-
+    public static final Logger LOGGER = LoggerFactory.getLogger("Dude, Whats My GePeUwu");
 
     @Override
     public void onInitializeClient() {
-        LOGGER.info("[" + MOD_NAME + "] Version " + VERSION + " loaded." );
-        LOGGER.info("[" + MOD_NAME + "] " + "GPU ready to be obliterated");
+
+        LOGGER.info("GPU ready to be obliterated");
     }
 }
--- src/main/java/one/devos/nautical/dudewhatsmygepeuwu/mixin/TitleScreenMixin.java
+++ src/main/java/one/devos/nautical/dudewhatsmygepeuwu/mixin/TitleScreenMixin.java
@@ -2,6 +2,7 @@ package one.devos.nautical.dudewhatsmygepeuwu.mixin;
 
 import com.mojang.blaze3d.platform.GlUtil;
 import com.mojang.blaze3d.vertex.PoseStack;
+import net.minecraft.SharedConstants;
 import net.minecraft.client.gui.Font;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.client.gui.screens.TitleScreen;
@@ -18,19 +19,18 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.util.function.Supplier;
 
-import static one.devos.nautical.dudewhatsmygepeuwu.client.DudeWhatsMyGePeUwuClient.MOD_NAME;
-
 // stereo im personally going to end you for using yarn mappings,
 // i had to slightly painfully translate this all to mojmap :crysis:
 // - aubee
 
+// to bad :troll:
+// i wasnt re-setting up my mod dev environment
+// - stereo
+
 @Mixin(TitleScreen.class)
 public class TitleScreenMixin extends Screen {
 
-	protected TitleScreenMixin(Font title) {
-		super((Component) title);
-	}
-
+	protected TitleScreenMixin(Font title) {super((Component) title);}
 	@Shadow @Final private boolean fading;
 	@Shadow private long fadeInStart;
 	Supplier<String> renderer = GlUtil::getRenderer;
@@ -42,12 +42,13 @@ public class TitleScreenMixin extends Screen {
 		float g = this.fading ? Mth.clamp(f - 1.0F, 0.0F, 1.0F) : 1.0F;
 		int l = Mth.ceil(g * 255.0F) << 24;
 
-		drawString(poseStack, this.font,"Current GePeUwu: " + renderer.get().split("/.*$")[0], 2, 2, 16777215 | l);
-
+		drawString(poseStack, this.font,"GPU: " + renderer.get().split("/.*$")[0], 2, 2, 16777215 | l);
 	}
 
 	@Inject(method = "init", at = @At("TAIL"))
 	protected void funnyGPUPrint(CallbackInfo info) {
-		DudeWhatsMyGePeUwuClient.LOGGER.info("[" + MOD_NAME + "] Current GePeUwu: " + renderer.get());
+		DudeWhatsMyGePeUwuClient.LOGGER.info("Current GePeUwu: " + renderer.get());
+		assert minecraft != null;
+		minecraft.getWindow().setTitle("Minecraft* - " + SharedConstants.getCurrentVersion().getName() + " " + renderer.get().split("/.*$")[0]);
 	}
 }
