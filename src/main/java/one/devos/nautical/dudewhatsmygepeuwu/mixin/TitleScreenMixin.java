a9625879d594044d59852c2575f368f007040a06
Asoji <asoji-git@hiitsdevin.mozmail.com>
2025-07-23 07:35:14 -0700

    ⬆️ 1.21.1 [1.1.0]

--- src/main/java/one/devos/nautical/dudewhatsmygepeuwu/mixin/TitleScreenMixin.java
+++ src/main/java/one/devos/nautical/dudewhatsmygepeuwu/mixin/TitleScreenMixin.java
@@ -1,17 +1,18 @@
 package one.devos.nautical.dudewhatsmygepeuwu.mixin;
 
 import com.mojang.blaze3d.platform.GlUtil;
-import com.mojang.blaze3d.vertex.PoseStack;
+import net.minecraft.Util;
 import net.minecraft.client.gui.Font;
+import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.client.gui.screens.TitleScreen;
 import net.minecraft.network.chat.Component;
 import net.minecraft.util.Mth;
-import net.minecraft.Util;
 import one.devos.nautical.dudewhatsmygepeuwu.client.DudeWhatsMyGePeUwuClient;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -22,38 +23,49 @@ import static one.devos.nautical.dudewhatsmygepeuwu.client.DudeWhatsMyGePeUwuCli
 
 // stereo im personally going to end you for using yarn mappings,
 // i had to slightly painfully translate this all to mojmap :crysis:
-// - aubee
+// - winter
 
 // to bad :troll:
 // i wasnt re-setting up my mod dev environment
 // - stereo
 
+// this feels like it fuckin sucks years later but oh well
+// - winter
+
 @Mixin(TitleScreen.class)
 public class TitleScreenMixin extends Screen {
 
-	protected TitleScreenMixin(Font title) {super((Component) title);}
-	@Shadow @Final private boolean fading;
-	@Shadow private long fadeInStart;
-	Supplier<String> renderer = GlUtil::getRenderer;
-	private boolean alreadyPrinted = false;
+    protected TitleScreenMixin(Font title) {
+        super((Component) title);
+    }
 
-	@Inject(method = "render", at = @At("TAIL"))
-	protected void funnyGPU(PoseStack poseStack, int xMouse, int yMouse, float delta, CallbackInfo ci) {
+    @Shadow
+    @Final
+    private boolean fading;
+    @Shadow
+    private long fadeInStart;
+    @Unique
+    Supplier<String> renderer = GlUtil::getRenderer;
+    @Unique
+    private boolean alreadyPrinted = false;
 
-		//can anyone please tell me if there's a way to shadow local variables
-		//im suffering in like 3 different mods where i have to do this lmao
-		float f = this.fading ? (float)(Util.getMillis() - this.fadeInStart) / 1000.0F : 1.0F;
-		float g = this.fading ? Mth.clamp(f - 1.0F, 0.0F, 1.0F) : 1.0F;
-		int l = Mth.ceil(g * 255.0F) << 24;
+    @Inject(method = "render", at = @At("TAIL"))
+    protected void funnyGPU(GuiGraphics guiGraphics, int mouseX, int mouseY, float delta, CallbackInfo ci) {
 
-		drawString(poseStack, this.font,"GPU: " + renderer.get().split("/.*$")[0], 2, 2, 16777215 | l);
-	}
+        //can anyone please tell me if there's a way to shadow local variables
+        //im suffering in like 3 different mods where i have to do this lmao
+        float f = this.fading ? (float) (Util.getMillis() - this.fadeInStart) / 1000.0F : 1.0F;
+        float g = this.fading ? Mth.clamp(f - 1.0F, 0.0F, 1.0F) : 1.0F;
+        int l = Mth.ceil(g * 255.0F) << 24;
 
-	@Inject(method = "init", at = @At("TAIL"))
-	protected void funnyGPUPrint(CallbackInfo info) {
-		if(!alreadyPrinted) {
-			DudeWhatsMyGePeUwuClient.LOGGER.info("[" + MOD_ID + "] Current GePeUwu: " + renderer.get());
-			alreadyPrinted = true;
-		}
-	}
+        guiGraphics.drawString(this.font, "GPU: " + renderer.get().split("/.*$")[0], 2, 2, 16777215 | l);
+    }
+
+    @Inject(method = "init", at = @At("TAIL"))
+    protected void funnyGPUPrint(CallbackInfo info) {
+        if (!alreadyPrinted) {
+            DudeWhatsMyGePeUwuClient.LOGGER.info("[" + MOD_ID + "] Current GePeUwu: " + renderer.get());
+            alreadyPrinted = true;
+        }
+    }
 }
